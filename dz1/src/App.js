43050a20e2a26a22a9e4008af133e3850636b912
Franklin <islamovshahridin@gmail.com>
2024-05-14 19:27:04 +0600

    dz1

---
+++ dz1/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+function Header (){
+  return(
+      <div>
+        <Title/>
+        <h2>Header</h2>
+      </div>
+  )
+}
+function Footer(){
+  return(
+      <div>
+        <Title/>
+        <h2>Footer</h2>
+      </div>
+  )
+}
+function Content (){
+  return(
+      <div>
+        <Title/>
+        <h2>Content</h2>
+      </div>
+  )
+}
+function Title(){
+  return(
+      <h3>Title</h3>
+  )
+}
+function App() {
+  return (
+      <div className="App">
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
+  );
+}
+
+export default App;
